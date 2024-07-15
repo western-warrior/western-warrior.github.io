42785e4538fd54af93d769253d6ce2828938305b
adi <@boomerang>
2024-07-15 18:20:38 -0500

    added script file

---
+++ script.js
@@ -0,0 +1,11 @@
+$("body").on("scroll", function () {
+    console.log(document.body.scrollTop);
+});
+
+//var menuClicked = false;
+//$("menu").click(
+//function () {
+//    if (menuClicked) {
+//        $("mobile-nav").show();
+//    }
+//});
