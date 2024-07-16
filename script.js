2940239aaa09e51b956d772adb8ef804dd353f43
adi <@boomerang>
2024-07-16 13:53:50 -0500

    example commit

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ document.getElementById("menu").addEventListener("click", function () {
     mob = document.getElementById("mobile-nav");
     mob.classList.remove("invisible");
     console.log("yes");
-}););
+});
