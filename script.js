8c72a4ac6f6a71a561edbd150cc468b7a60dddaf
adi <@boomerang>
2024-07-16 13:14:49 -0500

    example commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 $("menu").click(function () {
     $("mobile-nav").removeClass("invisible");
-    $("mobile-nav").addClass("animate__animated animate__slideInLeft");
+    $("mobile-nav").addClass("visible");
 });
